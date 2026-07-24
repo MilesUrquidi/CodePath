cfefbd30ce9d7a8e2b1450f3c976265ee5453c41
Miles Urquidi <milesurquidi@yahoo.com>
2026-07-24 12:27:59 -0700

    more problems

---
+++ Week6/part1/problem6.py
@@ -0,0 +1,30 @@
+"""
+You are working as an engineer normalizing volume levels on songs. 
+Given the head of a singly linked list with integer values song_audio representing volume levels at different points in a song, return the number of critical points. A critical point is a local minima or maxima.
+
+The head and tail nodes are not considered critical points.
+A node is a local minima if both the next and previous elements are greater than the current element
+A node is a local maxima if both the next and previous elements are less than the current element
+Evaluate the time and space complexity of your solution. Define your variables and provide a rationale for 
+why you believe your solution has the stated time and space complexity.
+"""
+
+class Node:
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
+
+# For testing
+def print_linked_list(head):
+    current = head
+    while current:
+        print(current.value, end=" -> " if current.next else "\n")
+        current = current.next
+
+def count_critical_points(song_audio):
+    pass
+
+
+song_audio = Node(5, Node(3, Node(1, Node(2, Node(5, Node(1, Node(2)))))))
+
+print(count_critical_points(song_audio))
